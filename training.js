420a8cb3044434fe6d5c657531f3da23de75f179
Usman Sharafa <Usmansharafa04@gmail.com>
2022-08-06 21:10:53 +0100

    Javascript logic for more info modal

--- training.js
+++ training.js
@@ -1,3 +1,9 @@
+const backdrop = document.getElementById("backdrop");
+console.dir(backdrop);
+function toggleBackdrop() {
+  backdrop.classList.toggle("visible");
+}
+
 document.addEventListener("click", (e) => {
   if (e.target.matches(".plus")) {
     isDropdown = true;
@@ -6,5 +12,26 @@ document.addEventListener("click", (e) => {
   } else if (e.target.matches(".question h4")) {
     e.target.nextElementSibling.classList.toggle("active");
     e.target.nextElementSibling.classList.toggle("rotate");
+  } else if (e.target.matches(".clarity")) {
+    toggleBackdrop();
+    const currentModal = e.target.nextElementSibling.nextElementSibling;
+    currentModal.classList.add("visible");
+    console.log(currentModal.classList);
+  } else if (e.target.matches("#backdrop")) {
+    const modal = e.target.nextElementSibling;
+    console.dir(modal);
+    toggleBackdrop();
+    function closeModal() {
+      modal.classList.remove("visible");
+    }
+    closeModal();
+  } else if (e.target.matches("#close")) {
+    const currentModal = e.target.parentElement.parentElement.parentElement;
+    function closeModal() {
+      currentModal.classList.remove("visible");
+    }
+    toggleBackdrop();
+    closeModal();
+    console.log(currentModal);
   }
 });
